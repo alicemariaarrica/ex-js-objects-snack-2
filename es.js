f0709df5246b6e6cb62262ae26ecb2de280d1019
alicemariaarrica <alicemariaarrica@gmail.com>
2025-04-02 22:19:50 +0200

    es2

--- es.js
+++ es.js
@@ -18,3 +18,25 @@ secondBurger.weight = 500;
 
 entrambi gli oggetti avranno come proprietà name = Double Cheese Burger
 
+
+
+
+Code Question 2
+const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+};
+
+const secondBurger = { ...hamburger };
+secondBurger.ingredients[0] = "Salad";
+
+console.log(hamburger.ingredients[0]); // ?
+console.log(secondBurger.ingredients[0]); // ?
+P.S.: Ricordati che gli Array, come gli oggetti, sono dei Reference Type(Tipi di Riferimento)!
+Senza lanciare il codice, riesci a prevedere cosa viene stampato in console ?
+    Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+
+
+Viene usato lo spread operator che copia e clona tutto tranne oggetti annidati ed array e funzioni.Perciò ha modificato la proprietà ingredients su entrambi.
+Essendo la proprietà ingredients un oggetto annidato. 
