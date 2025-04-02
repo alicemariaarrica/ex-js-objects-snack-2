c989c1ef04ebdb1aaa982ccddc7b84824ca400c5
alicemariaarrica <alicemariaarrica@gmail.com>
2025-04-02 22:23:51 +0200

    es4

--- es.js
+++ es.js
@@ -66,3 +66,28 @@ Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codic
 
 
 ogni oggetto contiene 2 oggetti annidati. 
+
+
+
+
+
+Code Question 4
+const chef = {
+    name: "Chef Hyur",
+    age: 29,
+    makeBurger: (num = 1) => {
+        console.log(`Ecco ${num} hamburger per te!`);
+    },
+}
+
+const restaurant = {
+    name: "Hyur's Burgers",
+    address: {
+        street: 'Main Street',
+        number: 123,
+    },
+    openingDate: new Date(2025, 3, 11),
+    isOpen: false,
+};
+Qual è il metodo migliore per clonare l’oggetto chef, e perché ? Lo spread operator perchè è l'unico che copia e crea funzioni.
+Qual è il metodo migliore per clonare l’oggetto restaurant, e perché ? structuredClone, perchè è l'unico che copia e crea oggetti speciali come Date
