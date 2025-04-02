2dd1af2a655dfad9510fe815c98a7ee5f465f786
alicemariaarrica <alicemariaarrica@gmail.com>
2025-04-02 22:20:47 +0200

    es3

--- es.js
+++ es.js
@@ -40,3 +40,26 @@ Senza lanciare il codice, riesci a prevedere cosa viene stampato in console ?
 
 Viene usato lo spread operator che copia e clona tutto tranne oggetti annidati ed array e funzioni.Perciò ha modificato la proprietà ingredients su entrambi.
 Essendo la proprietà ingredients un oggetto annidato. 
+
+
+
+
+
+Code Question 3
+const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    maker: {
+        name: "Anonymous Chef",
+        restaurant: {
+            name: "Hyur's Burgers",
+            address: "Main Street, 123",
+            isOpen: true,
+        },
+        age: 29
+    }
+};
+
+const secondBurger = structuredClone(hamburger);
+const thirdBurger = structuredClone(hamburger);
+Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice? 2. in tot sono 3
